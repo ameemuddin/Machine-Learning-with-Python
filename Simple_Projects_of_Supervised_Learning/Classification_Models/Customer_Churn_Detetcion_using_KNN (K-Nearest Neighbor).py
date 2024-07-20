cf827abb5803766a8c21c7a3aec6df792fb350ae
ameemuddin <ameemuddin19995@gmail.com>
2024-07-20 14:55:16 +0500

    Simple_Projects_of_Supervised_Learning

---
+++ Simple_Projects_of_Supervised_Learning/Classification_Models/Customer_Churn_Detetcion_using_KNN (K-Nearest Neighbor).py
@@ -0,0 +1,56 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+%matplotlib inline
+
+!wget -O teleCust1000t.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/teleCust1000t.csv
+
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
+
+df['custcat'].value_counts()
+
+df.hist(column='income', bins=50)
+
+df.columns
+
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+X[0:5]
+
+
+y = df['custcat'].values
+y[0:5]
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+k = 4
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+neigh
+
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+k = 6
+neigh6 = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+yhat6 = neigh6.predict(X_test)
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh6.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat6))
+
